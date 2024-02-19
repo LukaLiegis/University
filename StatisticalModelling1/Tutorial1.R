6bba2d3d388201ed01a7c53d1435ab8d95b65aeb
LukaLiegis <42875266+LukaLiegis@users.noreply.github.com>
2024-02-19 19:04:03 +0000

    Add files via upload

---
+++ StatisticalModelling1/Tutorial1.R
@@ -0,0 +1,30 @@
+x <- 3
+y <- 4
+z <- sqrt(x^2 + y^2)
+print(z)
+
+# Creating a vector
+x <- c(1, 2, 3)
+y <- c(4, 5, 6)
+c(x, y)
+
+# Calculating basic summary statistics
+sum(x)
+mean(x)
+median(x)
+sd(x)
+var(x)
+cor(x, y)
+cov(x, y)
+
+# 
+v <- c(11, 12, 13, 14, 15)
+w <- c(1, 2, 3, 4, 5)
+v + w
+v - w
+v * w
+v / w
+w + 2
+w / 2
+mean(w)
+w - mean(w)
